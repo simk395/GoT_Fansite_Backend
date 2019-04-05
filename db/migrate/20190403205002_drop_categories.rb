1fa158d50b18902d02f1842b9808080f1df4d276
simk395 <simon.kong95@gmail.com>
2019-04-05 10:27:22 -0400

    on
    Signed-off-by: simk395 <simon.kong95@gmail.com>

--- db/migrate/20190403205002_drop_categories.rb
+++
@@ -1,5 +0,0 @@
-class DropCategories < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :categories
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_205057) do
+ActiveRecord::Schema.define(version: 2019_04_03_165509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -29,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_205057) do
     t.boolean "pinned", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "profiles", force: :cascade do |t|
