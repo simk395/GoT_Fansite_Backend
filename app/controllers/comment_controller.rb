6475f271cd34c2ed821d9acd9653c148a7b70e1e
simk395 <simon.kong95@gmail.com>
2019-04-07 15:52:19 -0400

    on
    Signed-off-by: simk395 <simon.kong95@gmail.com>

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,2 +1,15 @@
 class CommentController < ApplicationController
+    def index
+        render json: Comment.all
+    end
+
+    def create
+        Comment.create(comment_params)
+    end
+
+    private
+
+    def comment_params
+        params.permit(:post_id, :profile_id, :message)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/categories', to: "category#index"
   get '/posts', to: "posts#index"
+  get '/comments', to: "comment#index"
+  post '/comments', to: "comment#create"
 end
